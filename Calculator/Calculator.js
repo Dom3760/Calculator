708b4db7e84dc5ead54f857132542ddc884b9130
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-01-11 10:28:31 -0800

    made more changes

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -11,6 +11,12 @@ display.textContent = 0
 var decimal = false
 var operater = false
 
+var add = false
+var subtract = false
+var multiply = false
+var divide = false
+var equal = false
+
 function clicked()
 {
     if (operater === false)
@@ -46,18 +52,42 @@ function clicked()
             if(buttonValue === "/")
             {
                 divide()
+                divide = true
+
+                multiply = false
+                subtract = false
+                add = false
             }
             else if(buttonValue === "*")
             {
                 multiply()
+                multiply = true
+
+                divide = false
+                subtract = false
+                add = false
             }
             else if(buttonValue === "-")
             {
                 subtract()
+                subtract = true
+
+                divide = false
+                multiply = false
+                add = false
             }
             else if(buttonValue === "+")
             {
                 add()
+                add = true
+
+                divide = false
+                multiply = false
+                subtract = false
+            }
+            else if(buttonValue === "=")
+            {
+                display.textContent = (currentNum)
             }
             else
             {
@@ -101,19 +131,24 @@ function clicked()
             // console.log("button clicked:", buttonValue)
             if(buttonValue === "/")
             {
-                divide()
+                divide = true
             }
             else if(buttonValue === "*")
             {
-                multiply()
+                multiply = true
             }
             else if(buttonValue === "-")
             {
-                subtract()
+                subtract = true
             }
             else if(buttonValue === "+")
             {
-                add()
+                add = true
+            }
+            else if(buttonValue === "=")
+            {
+                equal = true
+                answer()
             }
             else
             {
@@ -134,18 +169,55 @@ function divide()
     buttons[3].style.backgroundColor = "rgb(255, 195, 122)";
     operater = true
 
+    buttons[7,11,15].style.backgroundColor = "orange";
 }
 function multiply()
 {
+    display.innerText = newNum
+    buttons[7].style.backgroundColor = "rgb(255, 195, 122)";
+    operater = true
 
+    buttons[3,11,15].style.backgroundColor = "orange";
 }
 function subtract()
 {
+    display.innerText = newNum
+    buttons[11].style.backgroundColor = "rgb(255, 195, 122)";
+    operater = true
 
+    buttons[3,7,15].style.backgroundColor = "orange";
 }
 function add()
 {
+    display.innerText = newNum
+    buttons[15].style.backgroundColor = "rgb(255, 195, 122)";
+    operater = true
 
+    buttons[3,7,11].style.backgroundColor = "orange";
+}
+
+function answer ()
+{
+    if (add && equal === true)
+    {
+        currentNum = parseFloat(currentNum)+ parseFloat(newNum)
+        display.innerText = currentNum
+    }
+    if (subtract && equal === true)
+    {
+        currentNum = parseFloat(currentNum)- parseFloat(newNum)
+        display.innerText = currentNum   
+    }
+    if (multiply && equal === true)
+    {
+        currentNum = parseFloat(currentNum)* parseFloat(newNum)
+        display.innerText = currentNum
+    }
+    if (divide && equal === true)
+    {
+        currentNum = parseFloat(currentNum)/ parseFloat(newNum)
+        display.innerText = currentNum
+    }
 }
 
 console.log(buttons)
